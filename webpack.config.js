4d1deaaab5de470d667906697abee5ad92459aae
nilesh.patil@cuelogic.co.in <nilesh.patil@cuelogic.co.in>
2017-04-25 13:35:17 +0530

    webpack bundling changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     //entry point
     entry: path.resolve(__dirname, 'src') + '/app/index.js',
@@ -6,17 +8,28 @@ module.exports = {
     //output point 
     output: {
         path: path.resolve(__dirname, "dist") + '/app',
-        filename: 'bundle.js',
-        publicPath:'/app/'
+        filename: 'app.bundle.js',
+        publicPath: '/app/'
     },
     module: {
         loaders: [{
             test: /\.js$/,
             include: path.resolve(__dirname, 'src'),
             loader: 'babel-loader'
-        },{
+        }, {
             test: /\.css$/,
             loader: 'style-loader!css-loader'
         }]
-    }
+    },
+    plugins: [
+        //     new HtmlWebpackPlugin({
+        //     minify: {
+        //         collapseWhitespace: true
+        //     },
+        //     hash: true
+        // }),
+        new ExtractTextPlugin({
+            allChunks: true
+        })
+    ]
 };
